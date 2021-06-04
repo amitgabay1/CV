a185b6257ecc4eaf03b9da4f095e46132cbf85be
עמית <amitgaba@post.bgu.ac.il>
2021-06-04 19:44:34 +0300

    assignment 9

--- flaskProject3/app.py
+++ flaskProject3/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, url_for, session
-from flask import render_template, request
+from flask import render_template, request , redirect
 
 
 app = Flask(__name__,static_url_path='/static')
@@ -24,6 +24,37 @@ def assignment82():
     return render_template('/assignment_add.html',
     user = {'firstname': "amit", 'lastname': "gabay"},
     movies=['frind with benfit','bonnie and clayde','titanic'])
+
+
+
+@app.route('/assignment9', methods=['GET', 'POST'])
+def assignment9():
+    name = 'NONE'
+    Users = {"amit": "gabay", "ido": "gabay", "omer": "gabay", "lior": "ribak", "ariel": "amit"}
+    username = ' '
+    logged_in = True
+
+    if request.method == 'GET':
+        if 'name' in request.args:
+            name = request.args['name']
+
+    if request.method == 'POST':
+        username = request.form['username']
+        session['logged_in'] = True
+        session['username'] = username
+
+
+    return render_template('assignment9.html',
+                           request_method=request.method,
+                           name = name,
+                           Users = Users,
+                           username = username)
+
+@app.route('/log_out')
+def log_out():
+    session.pop('username')
+    session['logged_in'] = False
+    return redirect('/assignment9')
 
 
 if __name__ == '__main__':
